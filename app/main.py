af93a14db40285e607c374a99a347cfa849d109e
Yulya-S <yulya_Shabsnova_03@mail.ru>
2023-06-21 11:43:29 +0800

    change main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from pydantic import EmailStr
+from pydantic import EmailStr, BaseModel
 from fastapi import FastAPI
 
 api = FastAPI()
+
+class UserBase(BaseModel):
+    name:str
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    pass
+
+class UserIn(BaseModel):
+    password: str
 
 @api.get("/")
 async def index():
@@ -16,3 +27,7 @@ async def index(user_name: str | None = None):
 @api.post('/create_user')
 async def create_user(user_name: str, user_mail: EmailStr):
     return {'user_name': user_name, 'user_mail': user_mail}
+
+@api.post("/create_user_from_model", response_model=UserOut)
+async def create_user_from_model(user: UserBase):
+     return user
